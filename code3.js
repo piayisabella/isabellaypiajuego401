a8cfdba3e8b7da151932d1ce98b5519f76e52921
piayisabella <roblesgonzalezpia@gmail.com>
2026-04-17 10:36:39 -0700

    Add files via upload

---
+++ code3.js
@@ -0,0 +1,21 @@
+gdjs.escena_322Code = {};
+gdjs.escena_322Code.localVariables = [];
+gdjs.escena_322Code.idToCallbackMap = new Map();
+
+
+gdjs.escena_322Code.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.escena_322Code.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.escena_322Code.eventsList0(runtimeScene);
+
+
+return;
+
+}
+
+gdjs['escena_322Code'] = gdjs.escena_322Code;
